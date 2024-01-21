4125da205702e4175ac7d2f2caf423278ea70228
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2024-01-21 18:12:56 +0100

    first commit

---
+++ filter.js
@@ -0,0 +1,163 @@
+  
+/**
+ * To run this file in Gitpod, use the 
+ * command node filter.js in the terminal
+ */
+
+// .filter() --> RETURNS array
+
+// Simple Filtering
+const people = [
+  {
+    name: 'Michael',
+    age: 23,
+  },
+  {
+    name: 'Lianna',
+    age: 16,
+  },
+  {
+    name: 'Paul',
+    age: 18,
+  },
+];
+
+// WORKS... :
+// let filteredRes = people.filter(person => {
+//     if(person.age >= 18) {
+//       return person
+//     } 
+// });
+// console.log(filteredRes)
+
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough)
+/*returns
+[ { name: 'Michael', age: 23 } ]
+*/
+
+const justPaul = people.filter(p => p.name == 'Paul')
+console.log(justPaul)
+/*returns
+[ { name: 'Paul', age: 18 } ]
+*/
+
+/*-----------------------------------------------------------------*/
+// Complex Filtering
+const students = [
+  {
+    id: 1,
+    name: 'Mark',
+    profession: 'Developer',
+    skills: [
+      { name: 'javascript', yrsExperience: 1 },
+      { name: 'html', yrsExperience: 5 },
+      { name: 'css', yrsExperience: 3 },
+    ]
+  },
+  {
+    id: 2,
+    name: 'Ariel',
+    profession: 'Developer',
+    skills: [
+      { name: 'javascript', yrsExperience: 0 },
+      { name: 'html', yrsExperience: 4 },
+      { name: 'css', yrsExperience: 2 },
+    ]
+  },
+  {
+    id: 3,
+    name: 'Jason',
+    profession: 'Designer',
+    skills: [
+      { name: 'javascript', yrsExperience: 1 },
+      { name: 'html', yrsExperience: 1 },
+      { name: 'css', yrsExperience: 5 },
+    ]
+  },
+];
+
+// const candidates = students.filter(student =>{
+//   let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+//   return strongSkills.length > 0;
+// })
+// console.log(candidates)
+/*returns
+[
+  {
+    id: 1,
+    name: 'Mark',
+    profession: 'Developer',
+    skills: [ [Object], [Object], [Object] ]
+  },
+  {
+    id: 3,
+    name: 'Jason',
+    profession: 'Designer',
+    skills: [ [Object], [Object], [Object] ]
+  }
+]
+*/
+
+// SEPARATE .filter() to make SIMPLER TO READ:
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
+
+// GET ONLY candidate's names:
+const names = candidates.map(candidate => candidate.name);
+console.log(names);
+/*returns
+[ 'Mark', 'Jason' ]
+*/
+
+// EXAMPLE - filter student with maths >= 90
+let students2 = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 84, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 67, english: 87, art: 95},
+  }
+];
+
+const topMaths = students2.filter(student => student.results.maths >= 90);
+console.log(topMaths);
+/*returns
+[
+  {
+    name: 'John',
+    subjects: [ 'maths', 'english', 'cad' ],
+    teacher: { maths: 'Harry', english: 'Joan', cad: 'Paul' },
+    results: { maths: 90, english: 75, cad: 87 }
+  },
+  {
+    name: 'Adam',
+    subjects: [ 'science', 'maths', 'art' ],
+    teacher: { science: 'Iris', maths: 'Harry', art: 'Simon' },
+    results: { science: 84, maths: 97, art: 95 }
+  }
+]
+*/
+
+
+
